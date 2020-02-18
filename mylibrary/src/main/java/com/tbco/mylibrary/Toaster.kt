7d4f3e7be1526299da6dfb474c7968cb254f0fae
Mohammad Babaei <mohammadbabaeitb1997@gmail.com>
2020-02-18 12:06:05 +0330

    v0.0.2

--- mylibrary/src/main/java/com/tbco/mylibrary/Toaster.kt
+++ mylibrary/src/main/java/com/tbco/mylibrary/Toaster.kt
@@ -1,4 +1,10 @@
 package com.tbco.mylibrary
 
+import android.content.Context
+import android.widget.Toast
+
 object Toaster {
+    fun showToast(context: Context, message: String) =
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
 }
